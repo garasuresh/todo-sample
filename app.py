010f9c88782699bd4568825006bce3191be1ff9b
Suresh Gara <suresh.gara@pal.tech>
2025-05-01 18:10:26 +0530

    Updated app.py file with routes

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ data = {
   2: {"task": "Build todo", "done": False}
 }
 
-# @app.route("/todos", methods=["GET"])
-# def get_todos():
-#   response = []
-#   for key, value in data.items():
-#     temp = value
-#     temp["id"] = key 
-#     response.append(temp)
-#   return response
+@app.route("/todos", methods=["GET"])
+def get_todos():
+  response = []
+  for key, value in data.items():
+    temp = value
+    temp["id"] = key 
+    response.append(temp)
+  return response
 
 @app.route("/todos/<int:id>", methods=["GET"])
 def get_todo(id): 
